1d3a67aaa712c536d74897ec21cf6a081fd845a0
Martin Baute <solar@rootdirectory.de>
2026-01-05 09:10:21 +0100

    Config updates

--- system/etc/profile.d/clean_home.sh
+++ system/etc/profile.d/clean_home.sh
@@ -5,7 +5,7 @@ test -n "$XDG_CONFIG_HOME" && XDG_CONFIG_HOME="${HOME}/.config"; export XDG_CONF
 test -n "$XDG_CACHE_HOME"  && XDG_CACHE_HOME="${HOME}/.cache"; export XDG_CACHE_HOME
 test -n "$XDG_DATA_HOME"   && XDG_DATA_HOME="${HOME}/.local/share"; export XDG_DATA_HOME
 test -n "$XDG_STATE_HOME"  && XDG_STATE_HOME="${HOME}/.local/state"; export XDG_STATE_HOME
-test -d "${HOME}/.local/bin" && PATH="${PATH}:${HOME}/.local/bin"; export PATH
+test -d "${HOME}/.local/bin" && PATH="${HOME}/.local/bin:${PATH}"; export PATH
 
 # Shell Configurations in XDG_CONFIG_HOME
 
