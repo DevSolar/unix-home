715742869e21336c1b33c423a2308b27b042a769
Martin Baute <solar@rootdirectory.de>
2026-08-30 17:23:22 +0200

    Fixed logic inversion in clean_home.sh

--- system/etc/profile.d/clean_home.sh
+++ system/etc/profile.d/clean_home.sh
@@ -1,22 +1,22 @@
 # XDG Base Directory Specification
 # https://specifications.freedesktop.org/basedir-spec/latest
 
-test -n "$XDG_CONFIG_HOME" && XDG_CONFIG_HOME="${HOME}/.config"; export XDG_CONFIG_HOME
-test -n "$XDG_CACHE_HOME"  && XDG_CACHE_HOME="${HOME}/.cache"; export XDG_CACHE_HOME
-test -n "$XDG_DATA_HOME"   && XDG_DATA_HOME="${HOME}/.local/share"; export XDG_DATA_HOME
-test -n "$XDG_STATE_HOME"  && XDG_STATE_HOME="${HOME}/.local/state"; export XDG_STATE_HOME
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}"
 test -d "${HOME}/.local/bin" && PATH="${HOME}/.local/bin:${PATH}"; export PATH
 
 # Shell Configurations in XDG_CONFIG_HOME
 
-test -r "${XDG_CONFIG_HOME:-${HOME}/.config}/sh/profile" && . ${XDG_CONFIG_HOME:-${HOME}/.config}/sh/profile
+test -r "${XDG_CONFIG_HOME}/sh/profile" && . "${XDG_CONFIG_HOME}/sh/profile"
 
 case ${SHELL} in
     */bin/bash)
-        test -r "${XDG_CONFIG_HOME:-${HOME}/.config}/sh/bash_profile" && . "${XDG_CONFIG_HOME:-${HOME}/.config}/sh/bash_profile"
+        test -r "${XDG_CONFIG_HOME}/sh/bash_profile" && . "${XDG_CONFIG_HOME}/sh/bash_profile"
         ;;
     */bin/ksh)
-        test -r "${XDG_CONFIG_HOME:-${HOME}/.config}/sh/ksh_profile" && . "${XDG_CONFIG_HOME:-${HOME}/.config}/sh/ksh_profile"
+        test -r "${XDG_CONFIG_HOME}/sh/ksh_profile" && . "${XDG_CONFIG_HOME}/sh/ksh_profile"
         ;;
     *)
         ;;
